29e3b182ac758558641aae5f646937431b3ad6d3
charrbit <40149235+charrbit@users.noreply.github.com>
2023-10-28 16:41:38 -0500

    feat: removing tolerance control

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ parser.add_argument('-np', '--nPoints', type=int, default=16, help='Number of po
 parser.add_argument('-ni', '--nIndividuals', type=int, default=50, help='Number of individuals in the population')
 parser.add_argument('-ng', '--nGenerations', type=int, default=100, help='Number of generations of evolution to simulate')
 parser.add_argument('-mp', '--mutationProb', type=float, default=0.1, help='Probability of an individual mutating')
-parser.add_argument('-t', '--tol', type=float, default=1e-12, help='Tolerence between successive generation average scores before accepting convergence')
+# parser.add_argument('-t', '--tol', type=float, default=1e-12, help='Tolerence between successive generation average scores before accepting convergence')
 args=parser.parse_args()
 
 # Set arguments
@@ -16,7 +16,7 @@ numPoints = args.nPoints
 numIndividuals = args.nIndividuals
 numGenerations = args.nGenerations
 mutationProb = args.mutationProb
-tol = args.tol
+# tol = args.tol
 
 # Display parsed arguments
 print('Genetic Algorithm Approach to the Thomson Problem\n')
@@ -31,10 +31,8 @@ population = Population(numPoints, numIndividuals, mutationProb)
 print('Initial population generated!\n')
 population.printBestIndividual()
 print('\n')
-prevAvgScore = 0
-while (population.currGeneration < numGenerations) and (abs(population.getAverageScore()-prevAvgScore) > tol):
+while (population.currGeneration < numGenerations):
     print('*** Starting Generation: ', population.currGeneration + 1, '***\n')    
-    prevAvgScore = population.getAverageScore()
     population.makeSelection()
     population.crossParents()
     mutCount = population.applyMutations()
