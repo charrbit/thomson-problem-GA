61c3115f5f9cfa784da6ce82ae6883aa85936b0b
charrbit <40149235+charrbit@users.noreply.github.com>
2023-11-05 14:11:51 -0600

    refactor: updating fitness and random point functions to use point objects

--- helperMethods.py
+++ helperMethods.py
@@ -1,10 +1,10 @@
 import numpy as np
 from bitstring import BitArray
 
-# Generate two random numbers in [0, 1] that represent the coordinates of a point on the unit sphere
-def generateRandomPoint(rng):
+def generateRandomPoint():
+    ''' Generates a random point on the unit sphere '''
     thetaReal, phiReal = rng.random(2)
-    return [thetaReal, phiReal]
+    return Point(thetaReal, phiReal)
 
 # Convert a float into a BitArray (Should be 64-bit)
 def floatToBitArray(floatToConvert):
@@ -46,16 +46,10 @@ def joinBitArrays(bitArrayList):
     return bitarray
 
 def electroStaticPotential(point1, point2):
-    ''' Calculates the electrostatic potential between two points where
-        the points are given as BitArrays each containing a thetaReal and phiReal value '''
-    # Extract thetaReal and phiReal values for both points
-    P1 = bitArrayToPoint(point1)
-    P2 = bitArrayToPoint(point2)
-    # Convert the points to numpy arrays to find the vector difference
-    diff = np.subtract(np.array(P1), np.array(P2))
-    # Convert thetaReal and phiReal values to spherical coordinates theta and phi
-    diff *= np.array([np.pi, 2*np.pi])
-    return 1 / np.linalg.norm(diff)
+    ''' Calculates the electrostatic potential between two points  '''
+    # Convert the points to spherical coordinates then find the distance between them
+    dist = np.subtract(np.array(point1.getPointSpherical()), np.array(point2.getPointSpherical()))
+    return 1 / np.linalg.norm(dist)
 
 def isValidPoint(pointToCheck):
     thetaReal, phiReal = bitArrayToPoint(pointToCheck)
