bc26667cd7a71681b24d612a40759c7c346bf1e0
charrbit <40149235+charrbit@users.noreply.github.com>
2023-11-05 14:17:55 -0600

    refactor: updating individual object to operate on a list of point objects

--- Individual.py
+++ Individual.py
@@ -22,95 +22,54 @@ class Individual:
     # Let the chromosome be the combination of all points in the configuration be represented by a (128*numPoints)-bit BitArray
 
     def __init__(self, numPoints):
-        self.chromosome = BitArray(bin='0b') # Empty
+        self.chromosome = []
         self.numPoints = numPoints
+        self.fitnessScore = 0
 
         # Generate a random chromosome for this individual
-        self.generateNewChromosome()
+        self.generateChromosome()
         # Set the initial fitness value
-        self.fitnessScore = 0
         self.updateFitness()
 
-    def generateNewChromosome(self):
-        # Clear the current chromosome (if it exists)
+    def generateChromosome(self):
+        ''' Generates a random list of points on the unit sphere '''
+        # Reset the current chromosome
         self.chromosome.clear()
         for i in range(self.numPoints):
-            # Generate a new random point on the unit sphere
-            newPoint = generateRandomPoint(rng)
-            # Convert the point into its binary representation (BitArray)
-            newPointBitArray = pointToBitArray(newPoint)
-            # Add it to the chromosome
-            self.chromosome.append(newPointBitArray)
+            # Generate a random point on the unit sphere and add it to the chromosome
+            chromosome.append(generateRandomPoint())
 
     def updateFitness(self):
-        ''' Calculates the total electrostatic potential for the configuration '''
-        # Seperate each point within the chromosome BitArray
-        points = splitBitArray(self.chromosome, self.numPoints)
-        
+        ''' Calculates the total electrostatic potential for the configuration of points'''
+        # Reset the current fitness score
+        self.fitnessScore = 0
         # Calculate the fitness score for each pair of points
-        fitnessScore = 0
-        for i in range(self.numPoints - 1):
-            for j in range(i+1, self.numPoints):
-                fitnessScore += electroStaticPotential(points[i], points[j])
-        self.fitnessScore = fitnessScore
+        for i in range(self.numPoints):
+            for j in range(i + 1, self.numPoints):
+                self.fitnessScore += electroStaticPotential(self.chromosome[i], self.chromosome[j])
 
     def mutate(self):
-        # Seperate the chromosome into a list of point BitArrays
-        points = splitBitArray(self.chromosome, self.numPoints)
-        # Pick a random point in the chromosome BitArray
-        pointIndex = rng.integers(0, self.numPoints - 1)
-        point = points[pointIndex]
-        # Pick a random index in the point BitArray to mutate
-        mutIndex = rng.integers(0, len(point.bin) - 1)
-        point.invert(mutIndex)
+        ''' Randomly changes the value of one of the chromosome point coordinates '''
+        # Pick a random point from the chromosome list
+        pointIndex = rng.integers(self.numPoints)
+        point = self.chromosome[pointIndex]
+        # Pick a random coordinate from the point
+        coordIndex = rng.integers(2)
+        coord = point.getPointBitArray()[coordIndex]
+        # Pick a random bit in the coordinate to mutate
+        coord.invert(rng.integers(len(coord)))
+        # Get the new mutated coordinate value
+        mutVal = coord.float64
+
+        # Update the point with the new coordinate
+        newPointCoords = point.getPoint()
+        newPointCoords[coordIndex] = mutVal
+        point.setPoint(newPointCoords)
         # Check if this mutation lead to a coordinate value outside the acceptable range of [0, 1]
-        if not isValidPoint(point):
-            # Generate a new valid point
-            point = pointToBitArray(generateRandomPoint(rng))
+        if not point.isValid():
+            point = generateRandomPoint()
+
         # Replace the old point in the list
-        points[pointIndex] = point
-        # Rebuild the chromosome
-        self.chromosome = joinBitArrays(points)
+        self.chromosome[pointIndex] = point
         # Update the fitness score for the new configuration
         self.updateFitness()
-
-    def plot(self):
-        # Generate grid points around the unit sphere
-        theta = np.linspace(0, np.pi, 16)
-        phi = np.linspace(0, 2*np.pi, 32)
-        # Convert the spherical grid points to Cartesian
-        x = np.outer(np.sin(theta), np.cos(phi))
-        y = np.outer(np.sin(theta), np.sin(phi))
-        z = np.outer(np.cos(theta), np.ones_like(phi))
-
-        # Generate the 3D figure
-        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-        ax.set(xticklabels=[],
-               yticklabels=[],
-               zticklabels=[],
-               title=f'Number of Points: {self.numPoints}')
-        # Add the unit sphere grid to the figure
-        ax.plot_wireframe(x, y, z, color='k', alpha=0.3)
-
-        # Add the points of the individual onto the surface of the sphere
-        points = splitBitArray(self.chromosome, self.numPoints)
-        points = [bitArrayToPoint(point) for point in points]
-        points = np.array(points) * np.array([np.pi, 2*np.pi])
-        theta, phi = points[:, 0], points[:, 1]
-        x, y, z = sphericalToCartesian(1, theta, phi)
-        ax.scatter(x, y, z, s=20, color='b')
-
-        # Show the figure
-        plt.show()
-
-    def print(self):
-        print(f'Fitness score: {self.fitnessScore}')
-        print(f'Number of points: {self.numPoints}')
-        # Get each point as the spherical theta and phi values
-        points = splitBitArray(self.chromosome, self.numPoints)
-        points = [bitArrayToPoint(point) for point in points]
-        points = np.array(points) * np.array([np.pi, 2*np.pi])
-        for i, point in enumerate(points):
-            print(f'Point {i}:')
-            print(f'\tTheta:{point[0]} rad')
-            print(f'\tPhi:{point[1]} rad')
