329fbefb4d6716f2caf35168a20376688eb4b6fe
charrbit <40149235+charrbit@users.noreply.github.com>
2023-11-05 14:22:38 -0600

    docs: updating comments to reflect recent changes to individual object

--- Individual.py
+++ Individual.py
@@ -8,18 +8,7 @@ rng = np.random.default_rng()
 
 class Individual:
     ''' An individual of the genetic algorithm consits of an approximate solution to the Thomson
-        problem for a given number of electrons (i.e. a set of points on the sphere) '''
-
-    # A point on a sphere, in a spherical coordinate system, can be described by three values
-        # P = (R, theta, phi)
-    # For the unit sphere, R = 1 => P = (1, theta, phi) => P(theta, phi)
-        # Note: theta in [0, pi] and phi in [0, 2pi]
-    # Then, a point on the unit sphere can be described with two real numbers, thetaReal and phiReal
-        # P(theta, phi) = P(thetaReal * pi, phiReal * 2pi) => P(thetaReal, phiReal) where thetaReal and phiReal in [0, 1]
-
-    # Let each real coordinate be represented by a 64-bit BitArray
-    # Let each point be represented by a 128-bit BitArray
-    # Let the chromosome be the combination of all points in the configuration be represented by a (128*numPoints)-bit BitArray
+        problem for a given number of electrons (i.e. a list of points on the unit sphere) '''
 
     def __init__(self, numPoints):
         self.chromosome = []
@@ -73,3 +62,44 @@ class Individual:
         self.chromosome[pointIndex] = point
         # Update the fitness score for the new configuration
         self.updateFitness()
+
+    def plot(self):
+        # Generate grid points around the unit sphere
+        theta = np.linspace(0, np.pi, 16)
+        phi = np.linspace(0, 2*np.pi, 32)
+        # Convert the spherical grid points to Cartesian
+        x = np.outer(np.sin(theta), np.cos(phi))
+        y = np.outer(np.sin(theta), np.sin(phi))
+        z = np.outer(np.cos(theta), np.ones_like(phi))
+
+        # Generate the 3D figure
+        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+        ax.set(xticklabels=[],
+               yticklabels=[],
+               zticklabels=[],
+               title=f'Number of Points: {self.numPoints}')
+        # Add the unit sphere grid to the figure
+        ax.plot_wireframe(x, y, z, color='k', alpha=0.3)
+
+        # Add the points of the individual onto the surface of the sphere
+        points = splitBitArray(self.chromosome, self.numPoints)
+        points = [bitArrayToPoint(point) for point in points]
+        points = np.array(points) * np.array([np.pi, 2*np.pi])
+        theta, phi = points[:, 0], points[:, 1]
+        x, y, z = sphericalToCartesian(1, theta, phi)
+        ax.scatter(x, y, z, s=20, color='b')
+
+        # Show the figure
+        plt.show()
+
+    def print(self):
+        print(f'Fitness score: {self.fitnessScore}')
+        print(f'Number of points: {self.numPoints}')
+        # Get each point as the spherical theta and phi values
+        points = splitBitArray(self.chromosome, self.numPoints)
+        points = [bitArrayToPoint(point) for point in points]
+        points = np.array(points) * np.array([np.pi, 2*np.pi])
+        for i, point in enumerate(points):
+            print(f'Point {i}:')
+            print(f'\tTheta:{point[0]} rad')
+            print(f'\tPhi:{point[1]} rad')
